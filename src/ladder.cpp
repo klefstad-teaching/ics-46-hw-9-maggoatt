2cb550c963c375aa15c45e6f35ee987577c43942
maggoatt <mazhang19@gmail.com>
2025-03-15 23:35:52 +0000

    completed gen word ladder. working on is adj.

---
+++ src/ladder.cpp
@@ -0,0 +1,77 @@
+#include "ladder.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <cctype>
+using namespace std;
+
+void error(string word1, string word2, string msg) {
+    cout << word1 << word2 << msg << endl;
+}
+
+void lowercase(string &word) {
+    for (char& c: word) {
+        c = tolower(c);
+    }
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    lowercase(begin_word);
+    lowercase(end_word);
+    vector<string> begin {begin_word};
+    
+    if (begin_word == end_word) { // check invalid input: beginning and end are the same!
+        error(begin_word, end_word, " are invalid.");
+        return begin;
+    }
+
+    queue<vector<string>> ladder_queue; // initialize ladder queue with initial stack
+    ladder_queue.push(begin);
+
+    set<string> visited;
+    visited.insert(begin_word);
+    while (!ladder_queue.empty()) {
+        ladder = ladder_queue.pop()
+        last_word = ladder.back(); // last element in the current ladder
+        for (auto word: word_list) {
+
+            if (is_adjacent(last_word, word)) {
+
+                if (!visited.contains(word)) {
+                    visited.insert(word);
+                    new_ladder = ladder;
+                    new_ladder.push_back(word);
+
+                    if (word == end_word) {
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                } 
+            }
+        }
+    }
+    cout << "No ladder found." << endl;
+    vector<string> none;
+    return none;
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    // two cases: one word is 1 char longer than the other or the words differ by one letter
+    size_t length1 = word1.length();
+    size_t length2 = word2.length();
+
+    if (word1 != word2) {
+
+    }
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
+
+void load_words(set<string> & word_list, const string& file_name);
+
+void print_word_ladder(const vector<string>& ladder);
+
+void verify_word_ladder();
